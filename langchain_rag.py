c7b1d33a6d156f90922acfa92d478237fe119aef
Jayce0625 <957099583@qq.com>
2024-11-03 21:29:42 +0800

    2024/11/3

--- langchain_rag.py
+++ langchain_rag.py
@@ -77,9 +77,10 @@ if args.benchmark:
         print(name, param.device)
 
     generated_ids = stream_generate(model, messages, tokenizer)  # 对输入进行格式化，执行流式推理
+    print("LLM response: ", end="")
     response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]  # 使用分词器的batch_decode方法将生成的ID解码回文本，并跳过特殊token
     print(f"LLM response: \033[92m{response}\033[0m\n")
-    
+
     # 一次性推理输出
     # response = model(messages)  # 前向推理
     # llm_response = response['response']  # 从response字典中提取出大模型的回复
@@ -129,6 +130,7 @@ while True:
     ]  # 根据RAG增强得到的prompt构建用户输入
 
     generated_ids = stream_generate(model, messages, tokenizer)  # 对输入进行格式化，执行流式推理
+    print("LLM response: ", end="")
     response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]  # 使用分词器的batch_decode方法将生成的ID解码回文本，并跳过特殊token
 
     # 一次性推理输出
